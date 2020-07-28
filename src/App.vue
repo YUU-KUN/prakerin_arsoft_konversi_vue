e6fb2a91979e49f376756bc0b8537a2b0f06de56
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2020-07-28 16:15:20 +0700

    done-notfixed

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" style="width:50px;">
+    <Konversi/>
+  </div>
+</template>
+
+<script>
+import Konversi from './components/Konversi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Konversi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Konversi.vue
@@ -0,0 +1,397 @@
+<template>
+  <div>
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+              <p class="card-title float-left">
+                <b>Management Produk</b>
+              </p>
+              <p class="card-description float-right">
+                <b-button
+                  variant="success "
+                  v-b-modal.modalEdit
+                  v-on:click="Add"
+                >
+                  <!-- <i class="mdi mdi-plus btn-icon-prepend"></i> -->
+                  <span> + </span>
+                  Produk
+                </b-button>
+              </p>
+              <div class="table-responsive">
+                <b-table striped hover :items="produks" :fields="fields">
+                  <template v-slot:cell(aksi)="data">
+                    <b-button
+                      size="sm"
+                      variant="success"
+                      v-on:click="TambahBerat(data.item)"
+                      v-b-modal.modalTambahBerat
+                    >
+                      <!-- <i class="mdi mdi-pencil btn-icon-prepend"></i>  -->
+                      <span class="material-icons"> + </span> </b-button
+                    >&nbsp;
+
+                    <b-button
+                      size="sm"
+                      variant="danger"
+                      v-on:click="KurangBerat(data.item)"
+                      v-b-modal.modalKurangBerat
+                    >
+                      <!-- <i class="mdi mdi-pencil btn-icon-prepend"></i>  -->
+                      <span class="material-icons"> - </span> </b-button
+                    >&nbsp;
+
+                    <b-button
+                      size="sm"
+                      variant="info"
+                      v-on:click="Edit(data.item)"
+                      v-b-modal.modalEdit
+                    >
+                      <i class="mdi mdi-pencil btn-icon-prepend"></i>
+                      Ubah </b-button
+                    >&nbsp;
+                    <b-button
+                      size="sm"
+                      variant="danger"
+                      v-on:click="Drop(data.item.id)"
+                    >
+                      <i class="mdi mdi-delete btn-icon-prepend"></i> Hapus
+                    </b-button>
+                  </template>
+                </b-table>
+
+                <b-pagination
+                  v-model="currentPage"
+                  :per-page="perPage"
+                  :total-rows="rows"
+                  align="center"
+                  v-on:input="pagination"
+                ></b-pagination>
+
+                <b-toast id="loadingToast" title="Processing Data" no-auto-hide>
+                  <b-spinner label="Spinning" variant="success"></b-spinner>
+                  <strong class="text-success">Loading...</strong>
+                </b-toast>
+
+                <!-- toast untuk tampilan message box -->
+                <b-toast id="message" title="Message">
+                  <strong class="text-success">{{ message }}</strong>
+                </b-toast>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <b-modal id="modalEdit" @ok="Save">
+      <template v-slot:modal-title>Form Edit Produk</template>
+      <form ref="form">
+        <div class="form-group">
+          <label for="nama_produk" class="col-form-label">Nama Produk</label>
+          <input
+            type="text"
+            name="nama_produk"
+            class="form-control"
+            id="nama_produk"
+            placeholder="Nama Produk"
+            v-model="nama_produk"
+          />
+        </div>
+        <div class="form-group">
+          <label for="berat_produk" class="col-form-label">Berat Produk</label>
+          <input
+            type="text"
+            name="berat_produk"
+            class="form-control"
+            id="berat_produk"
+            placeholder="Berat Produk"
+            v-model="berat_produk"
+          />
+        </div>
+        <div class="form-group" @keyup.enter="save">
+          <label for="satuan_berat" class="col-form-label">Satuan</label>
+          <select
+            class="form-control"
+            name="satuan_berat"
+            id="satuan_berat"
+            v-model="satuan_berat"
+            placeholder="Satuan berat"
+          >
+            <option value="kg" checked>Kg</option>
+            <option value="ons">Ons</option>
+            <option value="gram">Gram</option>
+            <option value="mg">Mg</option>
+          </select>
+        </div>
+      </form>
+    </b-modal>
+
+    <!-- MODAL UNTUK MENAMBAH BERAT PRODUK -->
+    <b-modal id="modalTambahBerat" @ok="Save">
+      <template v-slot:modal-title>Form Tambah Berat Produk</template>
+      <form ref="form">
+        <h4>{{ nama_produk }}</h4>
+        <div class="form-group">
+          <label for="berat_tambah" class="col-form-label"
+            >Berat {{ nama_produk }}</label
+          >
+          <input
+            type="text"
+            name="berat_tambah"
+            class="form-control"
+            id="berat_tambah"
+            placeholder="Tambah Berat Produk"
+            v-model="berat_tambah"
+          />
+        </div>
+        <div class="form-group" @keyup.enter="save">
+          <label for="satuan_berat" class="col-form-label">Satuan</label>
+          <select
+            class="form-control"
+            name="satuan_berat"
+            id="satuan_berat"
+            v-model="satuan_berat"
+            placeholder="Satuan Berat"
+          >
+            <option value="kg" checked>Kg</option>
+            <option value="ons">Ons</option>
+            <option value="gram">Gram</option>
+            <option value="mg">Mg</option>
+          </select>
+        </div>
+      </form>
+    </b-modal>
+
+    <!-- MODAL UNTUK MENGURANGI BERAT PRODUK -->
+    <b-modal id="modalKurangBerat" @ok="Save">
+      <template v-slot:modal-title>Form Kurang Berat Produk</template>
+      <form ref="form">
+        <h4>{{ nama_produk }}</h4>
+        <div class="form-group">
+          <label for="berat_kurang" class="col-form-label"
+            >Berat {{ nama_produk }}</label
+          >
+          <input
+            type="text"
+            name="berat_kurang"
+            class="form-control"
+            id="berat_kurang"
+            placeholder="Kurangi Berat Produk"
+            v-model="berat_kurang"
+          />
+        </div>
+        <div class="form-group" @keyup.enter="save">
+          <label for="satuan_berat" class="col-form-label">Satuan</label>
+          <select
+            class="form-control"
+            name="satuan_berat"
+            id="satuan_berat"
+            v-model="satuan_berat"
+            placeholder="Satuan Berat"
+          >
+            <option value="kg" checked>Kg</option>
+            <option value="ons">Ons</option>
+            <option value="gram">Gram</option>
+            <option value="mg">Mg</option>
+          </select>
+        </div>
+      </form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      id: "",
+      nama_produk: "",
+      berat_produk: "",
+      satuan_berat: "",
+      berat_tambah: "",
+      berat_kurang: "",
+      produks: [],
+
+      search: "",
+      action: "",
+      message: "",
+      currentPage: 1,
+      rows: 0,
+      perPage: 10,
+      key: "",
+      fields: ["nama_produk", "berat_produk", "satuan_berat", "aksi"],
+    };
+  },
+
+  methods: {
+    getData: function() {
+      let offset = (this.currentPage - 1) * this.perPage;
+      this.$bvToast.show("loadingToast");
+      this.axios
+        .get(
+          "http://127.0.0.1:8000/api/getProduk/" + this.perPage + "/" + offset
+        )
+        .then((response) => {
+          if (response.data.status == 1) {
+            this.$bvToast.hide("loadingToast");
+            this.produks = response.data.produk;
+            this.rows = response.data.count;
+          } else {
+            this.$bvToast.hide("loadingToast");
+            this.message = "Gagal menampilkan data Produk.";
+            this.$bvToast.show("message");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    pagination: function() {
+      if (this.search == "") {
+        this.getData();
+      } else {
+        this.searching();
+      }
+    },
+
+    Add: function() {
+      this.action = "insert";
+      this.nama_produk = "";
+      this.berat_produk = "";
+      this.satuan_berat = "";
+    },
+
+    TambahBerat: function(item) {
+      this.action = "tambahBerat";
+      this.id = item.id;
+      this.nama_produk = item.nama_produk;
+      this.berat_tambah = "";
+      this.satuan_berat = item.satuan_berat;
+    },
+
+    KurangBerat: function(item) {
+      this.action = "kurangBerat";
+      this.id = item.id;
+      this.nama_produk = item.nama_produk;
+      this.berat_tambah = "";
+      this.satuan_berat = item.satuan_berat;
+    },
+
+    Edit: function(item) {
+      this.action = "update";
+      this.id = item.id;
+      this.nama_produk = item.nama_produk;
+      this.berat_produk = item.berat_produk;
+      this.satuan_berat = item.satuan_berat;
+    },
+
+    Save: function() {
+      this.$bvToast.show("loadingToast");
+      if (this.action === "insert") {
+        let form = new FormData();
+        form.append("id", this.id);
+        form.append("nama_produk", this.nama_produk);
+        form.append("berat_produk", this.berat_produk);
+        form.append("satuan_berat", this.satuan_berat);
+
+        this.axios
+          .post("http://127.0.0.1:8000/api/createProduk", form)
+          .then((response) => {
+            this.$bvToast.hide("loadingToast");
+            if (this.search == "") {
+              this.getData();
+            } else {
+              this.searching();
+            }
+            this.message = response.data.message;
+            this.$bvToast.show("message");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else if (this.action === "tambahBerat") {
+        let form = {
+          berat_tambah: this.berat_tambah,
+          satuan_berat: this.satuan_berat,
+        };
+        this.axios
+          .put("http://localhost:8000/api/tambahBerat/" + this.id, form)
+          .then((response) => {
+            this.$bvToast.hide("loadingToast");
+            if (this.search == "") {
+              this.getData();
+            } else {
+              this.searching();
+            }
+            this.message = response.data.message;
+            this.$bvToast.show("message");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else if (this.action === "kurangBerat") {
+        let form = {
+          berat_kurang: this.berat_kurang,
+          satuan_berat: this.satuan_berat,
+        };
+        this.axios
+          .put("http://localhost:8000/api/kurangBerat/" + this.id, form)
+          .then((response) => {
+            this.$bvToast.hide("loadingToast");
+            if (this.search == "") {
+              this.getData();
+            } else {
+              this.searching();
+            }
+            this.message = response.data.message;
+            this.$bvToast.show("message");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        let form = {
+          nama_produk: this.nama_produk,
+          berat_produk: this.berat_produk,
+          satuan_berat: this.satuan_berat,
+        };
+        this.axios
+          .put("http://localhost:8000/api/updateProduk/" + this.id, form)
+          .then((response) => {
+            this.$bvToast.hide("loadingToast");
+            if (this.search == "") {
+              this.getData();
+            } else {
+              this.searching();
+            }
+            this.message = response.data.message;
+            this.$bvToast.show("message");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+
+    Drop: function(id) {
+      if (confirm("Apakah anda yakin ingin menghapus data ini?")) {
+        this.$bvToast.show("loadingToast");
+        this.axios
+          .delete("http://127.0.0.1:8000/api/hapusProduk/" + id)
+          .then((response) => {
+            this.getData();
+            this.$bvToast.hide("loadingToast");
+            this.message = response.data.message;
+            this.$bvToast.show("message");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+  },
+};
+</script>
